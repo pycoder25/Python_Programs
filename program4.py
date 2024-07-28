d525f7fb8322bbf4c2be1143a50449013c05edfe
pycoder25 <dl491945@gmail.com>
2024-07-28 14:19:41 -0600

    nuevo codigo

--- program4.py
+++ program4.py
@@ -1,38 +1,16 @@
+# YouTube Downloader
 
-# Encryption program
+from pytube import YouTube
 
-import random
-import string
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
 
-#Encrypt
-def encryption():
 
-    chars = " "+string.punctuation+string.digits+string.ascii_letters
-    chars = list(chars)
-    key = chars.copy()
-    random.shuffle(key)
-
-    plain_text = input("Enter a messasge to encrypt: ")
-    cipher_text = ""
-
-    for letter in plain_text:
-        index = chars.index(letter)
-        cipher_text += key[index]
-    
-    print(f"Original message: {plain_text}")
-    print(f"Encrypted message: {cipher_text}")
-
-encryption()
-
-# Decrypt Program
-"""
-cipher_text = input("Enter a message to decrypt: ")
-plain_text = ""
-
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
-
-print(f"Encrypted message: {cipher_text}")
-print(f"Original message: {plain_text}")
-"""
+link = input("Enter the YouTube video URL: ")
+Download(link)
